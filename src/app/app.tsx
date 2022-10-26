02769ff5598a6624cb76410a729b7337d320917a
vpopov <vpopov@naumen.ru>
2022-10-26 14:24:24 +0500

    add husky

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = (): ReactElement => <div className="hello">Hello React</div>;
+const App = (): ReactElement => <div className="hello">Hello React!</div>;
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
